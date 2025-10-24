e8e97870e370b1eaca70e3c05b70a3b66cd88cc2
marya.6t@gmail.com <marya.6t@gmail.com>
2025-10-24 12:00:09 +0330

    create handler and router by writing HealthCheck

---
+++ src/api/api.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"github.com/Maryam-Talebzadeh/Gollang_SaleCar_WebApi/src/api/routers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitServer() {
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
+	v1 := r.Group("/api/v1")
+	{
+		health := v1.Group("/health")
+
+		routers.Health(health)
+
+	}
+
+	r.Run(":5005")
+}
---
+++ src/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Maryam-Talebzadeh/Gollang_SaleCar_WebApi/src/api"
+
+func main() {
+	api.InitServer()
+}
